502e273e5cccb9cd509e200859a33fffa5b8e723
juyonLee00 <ljy012900@gmail.com>
2024-06-18 01:35:43 +0900

    Feat : 대화창 커서 추가

--- Assets/Scripts/DialogueBar.cs
+++ Assets/Scripts/DialogueBar.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueBar : MonoBehaviour
 {
+
+    [SerializeField] TextMeshProUGUI dialogueNPC;
+    [SerializeField] TextMeshProUGUI dialogueData;
+
+
     // Start is called before the first frame update
     void Start()
     {
