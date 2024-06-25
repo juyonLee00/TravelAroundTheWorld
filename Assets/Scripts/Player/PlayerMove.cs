393089357b94ff88d0c5d85e2c7339d288b08dc0
juyonLee00 <ljy012900@gmail.com>
2024-06-25 21:21:51 +0900

    Feat : 플레이어 이동 기능 추가

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed = 6f;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+        Debug.Log("HI");
+    }
+}
