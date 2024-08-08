13eaa14eaa881af176a9d8aafadd78171df526b5
epdlrnldudnj <kje0023@naver.com>
2024-08-08 21:54:35 +0900

    [feat] item

--- Assets/Scripts/ShopUi/Slot.cs
+++ Assets/Scripts/ShopUi/Slot.cs
@@ -7,18 +7,18 @@ using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
-    public Sprite itemImage; // 아이템 이미지
-    public string itemName;
+    //public Sprite selectedImage; // 아이템 이미지
+    //public string itemName;
     public string itemDescription; // 아이템 설명
 
-    public Image itemImageDisplay;
-    public TextMeshProUGUI itemNameDisplay;
+    //public Image selectedImageDisplay;
+    //public TextMeshProUGUI itemNameDisplay;
     public TextMeshProUGUI itemDescriptionDisplay;
 
     public void OnSlotClicked()
     {
-        itemImageDisplay.sprite = itemImage;
-        itemNameDisplay.text = itemName;
+        //selectedImageDisplay.enabled = true;
+        //itemNameDisplay.text = itemName;
         itemDescriptionDisplay.text = itemDescription; // 아이템 설명
 ;
         //shopManager.DisplayItemDetails(itemImage.sprite, itemDescription, this);
---
+++ Assets/Scripts/ShopUi/TestPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+
+    public int bean = 1000;
+    public bool[] list = new bool[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
