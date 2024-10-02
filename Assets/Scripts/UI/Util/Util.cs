d45cb56c4f0ad5ba516994a8122b8a4a342f2153
juyonLee00 <ljy012900@gmail.com>
2024-10-02 11:31:07 +0900

    Chore : Util 수정

--- Assets/Scripts/UI/Util/Util.cs
+++ Assets/Scripts/UI/Util/Util.cs
@@ -1,26 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
 
 public class Util : MonoBehaviour
 {
-    public static Util Instance { get; private set; }
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
-
    
 }
